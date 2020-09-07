7a843b3e1a5a4f283d8f0f59454b67e7e81cf63b
brunoFernandes <bwendel26999@gmail.com>
2020-09-07 17:52:38 -0300

    ...

--- questao3.py
+++ questao3.py
@@ -25,7 +25,6 @@
 # O(a) vencedor(a) foi Zeugma com nota 9.4!
 
 def verificaVencedor():
-    
     comprimento = 3
     nomes = []
     notas = []
@@ -33,25 +32,25 @@ def verificaVencedor():
     notaMaior = 0
     stringResultado = "O(a) vencedor(a) foi {} com nota {:.2f}!"
     stringEmpate = "Empate!!! Os vencedores foram: "
-    
+
     for i in range(comprimento):
         contador += 1
 
         nome = str(input('Informe nome do {}º participante: '.format(contador)))
         nota = int(input('Informe nota do {}º participante: '.format(contador)))
-        while(nota > 10 or nota < 0):
+        while (nota > 10 or nota < 0):
             nota = int(input('Informe nota do {}º participante: '.format(contador)))
 
         nomes.append(nome)
         notas.append(nota)
-    
+
     for i in range(comprimento):
-        if(notas[i] > notaMaior):
+        if (notas[i] > notaMaior):
             notaMaior = notas[i]
             stringResultado = stringResultado.format(nomes[i], notas[i])
             stringEmpate = stringResultado
 
-        elif(notas[i] == notaMaior):
+        elif (notas[i] == notaMaior):
             stringEmpate += "\n{} com nota {:.2f}".format(nomes[i], notas[i])
             stringResultado = stringEmpate
         else:
@@ -59,6 +58,7 @@ def verificaVencedor():
 
     print(stringResultado)
     return None
+
 
 # main
 verificaVencedor()
